9d1205874717601c23b2b946f1b90eb88cb3c7b9
CIGNITIGLOBAL\E001116 <qasanjiv@gmail.com>
2017-01-18 17:12:26 +0530

    file creation

---
+++ learn.sh
@@ -0,0 +1,10 @@
+myname='Sanjeev Singh'
+myos="Win7"
+myno=8
+echo "My name is $myname"
+echo "My os is $myos"
+echo "My number is $myno, can you see this number"
+echo -e "An apple a day keeps away \a\t\tdoctor\n"
+echo `expr 1 + 3`
+echo "Today is `date`"
+exit 0
